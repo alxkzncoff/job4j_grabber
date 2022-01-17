a147c44686c743958260e95dc582463bd9a612bb
Aleksandr <alx.kzncoff@gmail.com>
2022-01-17 11:17:21 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,55 @@
+package ru.job4j.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            Map.entry("янв", "01"),
+            Map.entry("фев", "02"),
+            Map.entry("мар", "03"),
+            Map.entry("апр", "04"),
+            Map.entry("май", "05"),
+            Map.entry("июн", "06"),
+            Map.entry("июл", "07"),
+            Map.entry("авг", "08"),
+            Map.entry("сен", "09"),
+            Map.entry("окт", "10"),
+            Map.entry("ноя", "11"),
+            Map.entry("дек", "12"));
+
+    private static final  DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d-MM-yy HH:mm");
+    private static final  DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("d-MM-yy");
+
+
+    /**
+     * Метод преобразует строку в дату.
+     * @param parse строка, которую необходимо преобразовать.
+     * @return Дата.
+     */
+    @Override
+    public LocalDateTime parser(String parse) {
+        String dateTime = null;
+        String date = parse.split(",")[0];
+        String time = parse.split(",")[1];
+        if ("сегодня".equals(date)) {
+
+            dateTime = LocalDate.now().format(DATE_FORMATTER)
+                    + time;
+        }
+        if ("вчера".equals(date)) {
+            dateTime = LocalDate.now().minusDays(1).format(DATE_FORMATTER)
+                    + time;
+        }
+        if (!"сегодня".equals(date) && !"вчера".equals(date)) {
+            dateTime = date.split(" ")[0]
+                    + "-" + MONTHS.get(date.split(" ")[1])
+                    + "-" + date.split(" ")[2]
+                    + time;
+        }
+        return LocalDateTime.parse(dateTime, FORMATTER);
+    }
+}
