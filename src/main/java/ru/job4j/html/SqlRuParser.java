afbf72302aa92a09ffa896707031c8e90360647f
Aleksandr <alx.kzncoff@gmail.com>
2022-01-18 17:14:23 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParser.java
+++ src/main/java/ru/job4j/html/SqlRuParser.java
@@ -2,49 +2,73 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.Post;
+import ru.job4j.utils.DateTimeParser;
 import ru.job4j.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParser {
+public class SqlRuParser implements Parse {
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParser(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     /**
      * Метод загружает детали поста.
      * @param link ссылка на пост.
      * @return Post
      */
-    public static Post detail(String link) {
+    @Override
+    public Post detail(String link) {
         Post result = new Post();
         try {
             Document doc = Jsoup.connect(link).get();
+            Elements tdTitle = doc.select(".messageHeader");
             Elements tdComments = doc.select(".msgBody");
             Elements tdDates = doc.select(".msgFooter");
-            SqlRuDateTimeParser dateTimeParser = new SqlRuDateTimeParser();
             result.setDescription(tdComments.get(1).text());
             result.setCreated(dateTimeParser.parser(
-                    tdDates.get(1).text().split(", ")[0]
+                    tdDates.get(0).text().split(", ")[0]
                             + ", "
-                            + tdDates.get(1).text().split(", ")[1].split(" ")[0]));
+                            + tdDates.get(0).text().split(", ")[1].split(" ")[0]));
+            result.setLink(link);
+            result.setTitle(tdTitle.get(0).text());
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
 
-    public static void main(String[] args) throws Exception {
-        for (int i = 0; i <= 5; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td: row) {
-                Element parent = td.parent();
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(parent.children().get(5).text());
+    /**
+     * Метод загружает список всех постов.
+     * @param link ссылка на раздел с вакансиями.
+     * @return List список постов.
+     */
+    @Override
+    public List<Post> list(String link) {
+        List<Post> result = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements tdLinks = doc.select(".postslisttopic");
+            for (int i = 3; i < tdLinks.size(); i++) {
+                Post post = detail(tdLinks.get(i).child(0).attr("href"));
+                result.add(post);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SqlRuParser parser = new SqlRuParser(new SqlRuDateTimeParser());
+        System.out.println(parser.list("https://www.sql.ru/forum/job-offers"));
     }
 }
