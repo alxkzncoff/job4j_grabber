c27e4e5deb2239acde49d17f9d2a2fefbf2654ba
Aleksandr <alx.kzncoff@gmail.com>
2022-02-10 16:26:24 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #262914]

--- src/test/java/ru/job4j/design/srp/ReportXMLTest.java
+++ src/test/java/ru/job4j/design/srp/ReportXMLTest.java
@@ -3,7 +3,10 @@ package ru.job4j.design.srp;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import static org.junit.Assert.*;
 
@@ -12,17 +15,17 @@ public class ReportXMLTest {
     @Test
     public void whenXMLGenerated() {
         MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        LocalDateTime date = LocalDateTime.ofInstant(now.toInstant(), now.getTimeZone().toZoneId());
-        Employee worker = new Employee("Alex", now, now, 150);
+        Calendar date = new GregorianCalendar(2022, Calendar.JANUARY, 1);
+        date.setTimeZone(TimeZone.getTimeZone(ZoneOffset.of("+3")));
+        Employee worker = new Employee("Alex", date, date, 150);
         store.add(worker);
         Report engine = new ReportXML(store);
         String actual = engine.generate(em -> true);
         StringBuilder expected = new StringBuilder()
                 .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n")
                 .append("<employee name=\"").append(worker.getName()).append("\"")
-                .append(" hired=\"").append(date).append("+03:00\"")
-                .append(" fired=\"").append(date).append("+03:00\"")
+                .append(" hired=\"2022-01-01T00:00:00+03:00\"")
+                .append(" fired=\"2022-01-01T00:00:00+03:00\"")
                 .append(" salary=\"").append(worker.getSalary()).append("\"")
                 .append("/>\n");
         assertEquals(expected.toString(), actual);
